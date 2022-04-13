6431b851e9af9c2f2c2cfa58938d8c42f87c5c35
mwelsh356 <97926292+mwelsh356@users.noreply.github.com>
2022-04-13 17:05:23 -0400

    finished working on addMember function

--- index.js
+++ index.js
@@ -11,4 +11,54 @@ const Intern = require('./lib/Intern');
 const teamArray = [];
 
 // Function to add team member
-const addMember()
+const addMember = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter the team member's name"
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please select the team member's role",
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the team member's ID"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the team member's email"
+        },
+    ])
+    // Function to differentiate the last prompt based on role
+    .then(function({name, role, id, email}) {
+        let roleinfo = "";
+        if (role === "Engineer") {
+            roleinfo = "Github username";
+        }
+        else if (role === "Intern") {
+            roleInfo = "school name";
+        }
+        else {
+            roleinfo = "work phone number"
+        }
+        inquirer.prompt ([
+         {
+            type: 'input',
+            name: 'roleinfo',
+            message: `Please enter the team member's ${roleinfo}`
+        },
+        {
+            type: 'list',
+            name: 'addTeam',
+            message: "Would you like to add more members?",
+            choices: ['Yes', 'No']
+        }
+        ])
+    })
+}
