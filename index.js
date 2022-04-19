83a33fe85e233b73c356d3efc379b87d75299524
mwelsh356 <97926292+mwelsh356@users.noreply.github.com>
2022-04-19 15:39:36 -0400

    added intern and manager options to addHtml

--- index.js
+++ index.js
@@ -131,11 +131,34 @@ function addHtml(member) {
                 <li class= "list-group-item">GitHub: ${gitHub}</li>
             </ul>
             <div>
-        </div>
-                `
+        </div>`;
         }
         else if (role === "Intern") {
             const school = member.getSchool();
+            data = `<div class= "col-6">
+            <div class= "card mx-auto mb-3" style= "width: 18rem">
+            <h5 class= "card-header">${name}<br /><br />Intern</h5>
+            <ul class= "list-group list-group-flush">
+                <li class= "list-group-item">ID: ${id}</li>
+                <li class= "list-group-item">Email Address: ${email}</li>
+                <li class= "list-group-item">School: ${school}</li>
+            </ul>
+            <div>
+        </div>`;
         }
+        else {
+            const workPhone = member.getworkPhone();
+            data = `<div class= "col-6">
+            <div class= "card mx-auto mb-3" style= "width: 18rem">
+            <h5 class= "card-header">${name}<br /><br />Manager</h5>
+            <ul class= "list-group list-group-flush">
+                <li class= "list-group-item">ID: ${id}</li>
+                <li class= "list-group-item">Email Address: ${email}</li>
+                <li class= "list-group-item">School: ${workPhone}</li>
+            </ul>
+            <div>
+        </div>`;
+        }
+        console.log
     })
 }
