36f7d0dd1749f03070675acb727649bb00140440
mwelsh356 <97926292+mwelsh356@users.noreply.github.com>
2022-04-26 18:09:41 -0400

    finished working on the intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -2,3 +2,23 @@
 const Intern = require('../lib/Intern');
 
 // Creating intern object
+ test('creates intern object', () => {
+     const intern = new Intern('Mike', 90, 'michaelwelsh356@gmail.com', 'tOSU');
+
+     expect(intern.school).toEqual(expect.any(String));
+ });
+
+ // Tests to see if it can get the school
+ test('gets school name', () => {
+     const intern = new Intern('Mike', 90, 'tOSU');
+
+     expect(intern.getSchool()).toEqual(expect.stringContainer(intern.school.toString()));
+ });
+
+ // Tests to see if it gets the role 
+ test('gets role of employee', () => {
+     const intern = new Intern('Mike', 90, 'tOSU');
+
+     expect(intern.getRole()).toEqual('Intern');
+ });
+
